ec67ad25b866f3b718d20031289b4d7251a02d5a
Jonatan Chavarriaga <jonatanchavarriaga@gmail.com>
2024-09-03 16:40:31 -0500

    Added app

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [data, setData] = useState([]);
+  const URL = 'https://rickandmortyapi.com/api/character';
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.info}>Status: {item.status}</Text>
+      </View>
     </View>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={data} keyExtractor={(item) => item.id.toString()} renderItem={renderItem} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 40,
+    backgroundColor: '#000814',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: '#fdc500',
+    marginBottom: 10,
+    borderRadius: 8,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  textContainer: {
+    marginLeft: 15,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 16,
+    color: '#979DAC',
   },
 });
+
+export default App;
